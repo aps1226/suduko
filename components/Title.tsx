060c2c6adf0ec98bab20003c81b230323a25310a
aps1226 <asheehy1226@gmail.com>
2021-09-01 16:17:01 -0400

    Added a title to the home page. Improved styling on the home page popup menus.

---
+++ components/Title.tsx
@@ -0,0 +1,31 @@
+import React,{ useEffect } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+import TextStroke from './TextStroke';
+export default function Title() {
+
+  return (
+    <View style = {styles.container}>
+      <TextStroke stroke = {2} color = {'white'}>
+        <Text style = {styles.winnerText}>
+          Suduko
+        </Text>
+      </TextStroke>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection:'row',
+    position:'absolute',
+    top:'30%'
+  },
+  winnerText:{
+    fontSize:80,
+    color:'#1E90FF',
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    elevation:3,
+  }
+});
